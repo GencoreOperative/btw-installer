44653fd4960bd73f9ac03f4c6ce0145a2067ec09
Robert Wapshott <robert@wapshott.org.uk>
2017-09-04 17:13:46 +0100

    Identifying the relationships between tasks

--- src/main/java/uk/co/gencoreoperative/btw/Main.java
+++ src/main/java/uk/co/gencoreoperative/btw/Main.java
@@ -21,6 +21,8 @@ import static uk.co.gencoreoperative.btw.utils.FileUtils.*;
 
 // Based on  http://www.sargunster.com/btwforum/viewtopic.php?f=9&t=8925
 public class Main {
+    private static final Predicate<File> EXISTS = File::exists;
+
     private static final MineCraftPathResolver FOLDER = new MineCraftPathResolver("/tmp/badger");
     private static final String PATCH_FOLDER = "MINECRAFT-JAR/";
 
@@ -29,29 +31,38 @@ public class Main {
         Arrays.stream(Tasks.values()).forEach(tasks -> progress.addItem(tasks.getTask()));
 
         // Verify that 1.5.2 version is present.
-        validate(Tasks.ONE_FIVE_TWO_EXISTS, FOLDER.oneFiveTwo(),
-                folder -> folder.exists() && new File(folder, "1.5.2.jar").exists());
+        // TODO: No action, validate folder and sub file exist
+        validate(Tasks.ONE_FIVE_TWO_EXISTS, new File(FOLDER.oneFiveTwo(), "1.5.2.jar"), EXISTS);
 
+        // TODO: Request from user patch location, validate file exists
         File patchFile = FileChooser.requestLocation("patch.location", new File(System.getProperty("user.home")));
-        validate(Tasks.PATCH_WAS_SELECTED, patchFile, file -> file != null && file.exists());
+        // TODO: Handle null response
+        validate(Tasks.PATCH_WAS_SELECTED, patchFile, EXISTS);
 
         // Remove previous installation
+        // TODO: If folder exists delete it - validate folder does not exist
         File targetFolder = FOLDER.betterThanWolves();
         if (targetFolder.exists()) {
             FileUtils.recursiveDelete(targetFolder);
-            validate(Tasks.PREVIOUS_REMOVED, targetFolder, file -> !file.exists());
+            validate(Tasks.PREVIOUS_REMOVED, targetFolder, EXISTS.negate());
         }
 
         // Create target folder
+        // TODO: If folder did not exist create it - validate folder exists
         targetFolder.mkdirs();
         validate(Tasks.CREATED_FOLDER, targetFolder, File::exists);
 
         // Copy JSON from resources - we do not expect this to change
+        // TODO: Read static resource and write to file - validate file exists
         File targetJson = new File(targetFolder, "BetterThanWolves.json");
         FileUtils.copyStream(Main.class.getResourceAsStream("/4-A2/BetterThanWolves.json"),
                 write(targetJson),
                 true, true);
-        validate(Tasks.COPIED_JSON, targetJson, File::exists);
+        validate(Tasks.COPIED_JSON, targetJson, EXISTS);
+
+        // Open target jar
+        // TODO: Create target jar and apply patch - validate file exists
+        File targetJarFile = new File(targetFolder, "BetterThanWolves.jar");
 
         // stream the contents of the BTW Patch utils into a map
         Map<String, EntryAndData> modFiles = new HashMap<>();
@@ -60,8 +71,6 @@ public class Main {
                 .map(entry -> EntryAndData.file(trimModPath(entry.getEntry()), entry.getData()))
                 .forEach(entry -> modFiles.put(entry.getName(), entry));
 
-        // Open target jar
-        File targetJarFile = new File(targetFolder, "BetterThanWolves.jar");
         try (ZipOutputStream targetStream = openZip(targetJarFile)) {
             // Define function for streaming contents to target jar.
             Consumer<EntryAndData> copyToTargetJar = entry -> {
@@ -88,7 +97,7 @@ public class Main {
         } catch (IOException e) {
             throw new IllegalStateException(e);
         }
-        validate(Tasks.COPIED_JAR, targetJarFile, File::exists);
+        validate(Tasks.COPIED_JAR, targetJarFile, EXISTS);
 
 
 //        // Signal User
